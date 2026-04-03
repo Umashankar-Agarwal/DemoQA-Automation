ec74b9bc19a5d2a8f16008d23d0b3b85a9747b7a
Umashankar Agarwal <65114202+Umashankar-Agarwal@users.noreply.github.com>
2026-04-03 14:15:30 +0530

    Refactor JavaScriptExecutor and WebDriverWait usage

--- src/main/java/utils/Reusuables.java
+++ src/main/java/utils/Reusuables.java
@@ -9,39 +9,36 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Reusuables {
-
-    private static JavascriptExecutor js;
-    private static WebDriverWait wait;
-
+    
     public static void scrollIntoView(WebElement element, WebDriver driver) {
-        js = (JavascriptExecutor) driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         // Scrolls the element into view, aligning it to the top of the viewport (true)
         js.executeScript("arguments[0].scrollIntoView(true)", element);
     }
 
     public static void scrollToBottom(WebDriver driver){
-        js = (JavascriptExecutor) driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
     }
 
     public static void hiddenElementClick(WebDriver driver, WebElement element){
-        js = (JavascriptExecutor) driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();",element);
     }
 
     public static void scrollTo500(WebDriver driver){
-        js = (JavascriptExecutor) driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0, 500)");
     }
 
     public static void scrollToTop(WebDriver driver){
-        js = (JavascriptExecutor) driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0, 0);");
     }
 
 
     public static void waitElementClickable(WebElement element, WebDriver driver,int seconds){
-        wait = new WebDriverWait(driver,Duration.ofSeconds(seconds));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(seconds));
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 }
